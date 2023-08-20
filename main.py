d840d45975488173ef77273f11ba7b4559aee760
Simant <developersimant@gmail.com>
2023-08-20 21:43:39 +0545

    Added Salary

--- main.py
+++ main.py
@@ -13,47 +13,53 @@ words = text.lower().split()
  
  #for math subject
 if "math" in words and "master" in words: 
-    print("eligible for bachleor teaching")
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
 
 if "math" in words and "bachelor" in words: 
-    print("eligible for +2 teaching")
+    print("Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
 
 
 #nepali
 if "Nepali" in words and "bachleor" in words:
-    print("eligible for 11 12 teaching")
+    print("Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
 if "Nepali" in words and "master" in words:
-    print("eligible for Bachelor teaching")
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
+
 
 #for social
 if "social" in words and "master" in words:
-    print("eligible for bachelor teching")
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
 if "social" in words and "bachleor" in words:
-    print("eligible for 11 12 teaching")
+    print("Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
 
 #for Science 
 if "Science" in words and "master" in words:
-    print("eligible for Bachelor teching")
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
 if "Science" in words and "bachleor" in words:
-    print("eligible for 11 12 teaching")
+    print("Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
 
 #for computer
-if "computer" in words and "master" in words:
-    print("eligible for Bacheloer teching")
-if "computer" in words and "bachleor" in words:
-    print("eligible for 11 12 teaching")
- 
+if "computer Science" in words and "master" in words:
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
+if "computer Science" in words and "bachleor" in words:
+    print("Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
+
+#For Information technology Subject
+if "Bachelor in Information Technology" in words and "master" in words:
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
+if " master in Information Technology" in words and "bachleor" in words:
+    print("Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
+  
 #for HPE
 if "Health & population" in words and "master" in words:
-    print("eligible for Bacheloer teching")
+    print("Congratulation you're eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
 if "Health & population" in words and "bachleor" in words:
-    print("eligible for 11 12 teaching")
+    print(" Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
 
 #for Account
 if "Business Studies" in words and "master" in words:
-    print("eligible for Bacheloer teching")
+    print("eligible upto Bacheloer teching and your salary is upto 40000-50000 or negotiable")
 if "Health & population" in words and "bachleor" in words:
-    print("eligible for 11 12 teaching")
-
+    print(" Congratulation you're eligible upto 11 12 teaching and your salary is 16000-17000")
 else:
-    print("not eligible")
+    print(" sorry ! you're not eligible")
